92cbb0e29f81390c5ca4b761bc31279269c77e1b
iryna <iryna.dubenets@yahoo.com>
2022-02-04 13:07:57 -0600

    practice continue

---
+++ src/test/java/com/cydeo/tests/T2_Linktext_Practice.java
@@ -0,0 +1,54 @@
+package com.cydeo.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_Linktext_Practice {
+
+    public static void main(String[] args) {
+
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to: https://practice.cydeo.com
+        driver.get("https://practice.cydeo.com");
+
+        //3. Click to A/B Testing from top of the list.
+        driver.findElement(By.linkText("A/B Testing")).click();
+
+        //4. Verify title is:
+        //Expected: No A/B Test
+        String expectedTitle = "No A/B Testing";
+        String actualTitle = driver.getTitle();
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification PASSED!");
+        }else{
+            System.out.println("Title verification FAILED!");
+        }
+
+        //5. Go back to home page by using the .back();
+        driver.navigate().back();
+
+        //6. Verify title equals:
+        //Expected: Practice
+        String expectedTitle2 = "Practice";
+        actualTitle = driver.getTitle();
+        if(actualTitle.equals(expectedTitle2)){
+            System.out.println("Title verification PASSED!");
+        }else{
+            System.out.println("Title verification FAILED!");
+        }
+
+
+
+
+
+
+
+    }
+
+}
